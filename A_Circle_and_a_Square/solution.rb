9b584860867c4d5c779b673a429d29ff00ac62b1
RicardoG2016 <gomez.r@live.com>
2017-02-24 02:09:29 -0600

    complete square function and circle function to check for pixels inside shapes. Both work together for initial test provided.

--- A_Circle_and_a_Square/solution.rb
+++ A_Circle_and_a_Square/solution.rb
@@ -24,9 +24,40 @@ x1 = 16
 y1 = 14
 x3 = 8
 y3 = 14
-
+rows = h
 
 canvas = Array.new(h) { Array.new(w, ".")}
+
+def in_sqr(array, x1, y1, x3, y3, h)
+  y2 = y1
+  if y1 == y3
+    count = (x1 - x3).abs + 1
+    top = (count)/2
+    beg = x1 < x3 ? x1 : x3
+    i = 0
+    up = 0
+    down = 1
+
+    if y1 - 1 >=0
+      while i < top + 1 do
+        j = i
+        while  j < count do
+          array[y1][beg + j] = "#"
+          if y2 < h
+           array[y2][beg + j] = "#" 
+          end
+          j+=1
+        end
+        i+=1
+        count-=1
+        y1-= 1
+        y2+= 1
+        down = y1 + 1
+      end
+    end     
+  end
+  p array
+end
 
 def in_circle(array, r, circleX, circleY)
   r_sq = r ** 2
@@ -36,7 +67,6 @@ def in_circle(array, r, circleX, circleY)
       arrayY = y
       x_net_sq = (arrayX - circleX) ** 2
       y_net_sq = (arrayY - circleY) ** 2
-
     if r_sq >= x_net_sq + y_net_sq
       array[y][x] = "#"
     end
@@ -45,14 +75,6 @@ def in_circle(array, r, circleX, circleY)
   }
 end
 
-in_circle(canvas, r, circleX, circleY)
+newArray = in_circle(canvas, r, circleX, circleY)
 
-
-
-
-
-
-
-
-
-
+in_sqr(newArray, x1, y1, x3, y3, h)
