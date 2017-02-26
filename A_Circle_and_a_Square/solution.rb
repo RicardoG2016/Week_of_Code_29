8aae245b29121bb28874064b95ccc9a6de2c17ef
RicardoG2016 <gomez.r@live.com>
2017-02-25 22:47:12 -0600

    added functiont to check square based on mid points and refactored code. Also added constraints.

--- A_Circle_and_a_Square/solution.rb
+++ A_Circle_and_a_Square/solution.rb
@@ -1,3 +1,4 @@
+require 'pry'
 # Provided code
 # --------------------------------------------------
 # w,h = gets.strip.split(' ')
@@ -14,7 +15,7 @@
 # y3 = y3.to_i
 
 # your code goes here
-
+square_drawn = false
 w = 20
 h = 16
 circleX = 9
@@ -24,12 +25,73 @@ x1 = 16
 y1 = 14
 x3 = 8
 y3 = 14
+sqr_c_x = (x1 + x3) / 2
+sqr_c_y = (y1 + y3) / 2
+square_details = { :point1 => [x1, y1], :point2 => [], :point3 => [x3, y3], :point4 => [], :center => [sqr_c_x, sqr_c_y]}
 
 canvas = Array.new(h) { Array.new(w, ".")}
 
-def in_sqr(array, x1, y1, x3, y3, h, w)
-  y2 = y1
-  if y1 == y3
+def complete_square(square_details)
+  x1 = square_details[:point1][0] 
+  y1 = square_details[:point1][1]
+  x3 = square_details[:point3][0] 
+  y3 = square_details[:point3][1]
+  center_x = square_details[:center][0]
+  center_y = square_details[:center][1]
+
+  slope_x = center_x - ( x1 < x3 ? x1 : x3 )
+  slope_y = center_y - ( y1 < y3 ? y1 : y3 )
+  slope_c = [slope_x, slope_y]
+
+  x2 = center_x - (slope_y).abs 
+  y2 = center_y - (slope_x).abs 
+  
+  x4 = center_x + (slope_y).abs 
+  y4 = center_y + (slope_x).abs 
+
+  square_details[:point2][0] = x2
+  square_details[:point2][1] = y2
+  square_details[:point4][0] = x4
+  square_details[:point4][1] = y4
+end
+
+def in_circle(array, r, circleX, circleY)
+  if r >= 0 && r <= 100 && circleX >= -100 && circleY >= -100 && circleX <= 200 && circleY <= 200
+    r_sq = r ** 2
+    array.each_index { |y| 
+      array[y].each_index { |x|
+        arrayX = x
+        arrayY = y
+        x_net_sq = (arrayX - circleX) ** 2
+        y_net_sq = (arrayY - circleY) ** 2
+      if r_sq >= x_net_sq + y_net_sq
+        array[y][x] = "#"
+      end
+      }
+    }
+  end
+end
+
+def x_test(array, x1, y1, x3, y3, h, w, square_drawn)
+  if x1 == x3 
+    mid_y = (y1 + y3) /2
+    mid_x = x1
+    dis_to_mid = ((mid_y).abs - (y1).abs)
+    l_y = mid_y
+    l_x = x1 - dis_to_mid
+    r_x = x1 + dis_to_mid
+    r_y = mid_y
+    x_scan = y_test(array,l_x, l_y, r_x, r_y, h, w, square_drawn)
+    square_drawn = true
+    return x_scan
+  else
+    return array
+  end
+end
+
+def y_test(array, x1, y1, x3, y3, h, w, square_drawn)
+  z1 = y1
+  if y1 == y3 && square_drawn == false
     count = (x1 - x3).abs + 1
     top = (count)/2
     beg = x1 < x3 ? x1 : x3
@@ -40,62 +102,77 @@ def in_sqr(array, x1, y1, x3, y3, h, w)
         j = i
         while  j < count do
           array[y1][beg + j] = "#"
-          if y2 <= h-1
-           array[y2][beg + j] = "#" 
+          if z1 <= h-1
+           array[z1][beg + j] = "#" 
           end
           j+=1
         end
         i+=1
         count-=1
         y1-= 1
-        y2+= 1
+        z1+= 1
       end
-    end     
+    end 
+  square_drawn = true    
   end
+  return array
+end
 
-  if x1 == x3
-    count = (y1 - y3).abs + 1
-    top = (count)/2
-    beg = y1 < y3 ? y1 : y3
-    i = 0
+# square_details = { :point1 => [x1, y1], :point2 => [x3, y3], :center => [sqr_c_x, sqr_c_y] }
 
-    if x1 + 1 <= w - 1
-      while i < top + 1 do
-        j = i
-        while  j < count do
-          array[y1 + j][beg] = "#"
-          if x2 < w
-           array[x2 + j][beg + j] = "#" 
-          end
-          j+=1
+def mid_points(array, square_details, square_drawn)
+# points
+  x1 = square_details[:point1][0] 
+  y1 = square_details[:point1][1]
+  x2 = square_details[:point2][0] 
+  y2 = square_details[:point2][1]
+  x3 = square_details[:point3][0] 
+  y3 = square_details[:point3][1]
+  x4 = square_details[:point4][0] 
+  y4 = square_details[:point4][1]
+# mid-points
+  mid1x = (x1 + x2) / 2
+  mid1y = (y1 + y2) / 2
+  mid2x = (x2 + x3) / 2
+  mid2y = (y2 + y3) / 2
+  mid3x = (x3 + x4) / 2
+  mid3y = (y3 + y4) / 2
+  mid4x = (x1 + x4) / 2
+  mid4y = (y1 + y4) / 2
+
+  mid_x = [mid1x, mid2x, mid3x, mid4x].sort
+  mid_y = [mid1y, mid2y, mid3y, mid4y].sort
+
+if square_drawn == false
+  array.each_with_index do |row, y|
+    row.each_with_index do |point, x|
+        if mid_x[0] < x && x < mid_x[3] && mid_y[0] <= y && y <= mid_y[3]
+          point = "#"
         end
-        i+=1
-        count-=1
-        x1-= 1
-        x2+= 1
-      end
-    end     
-  end
-  array.each do |x|
-    puts x.flatten.join
-  end
-end
-
-def in_circle(array, r, circleX, circleY)
-  r_sq = r ** 2
-  array.each_index { |y| 
-    array[y].each_index { |x|
-      arrayX = x
-      arrayY = y
-      x_net_sq = (arrayX - circleX) ** 2
-      y_net_sq = (arrayY - circleY) ** 2
-    if r_sq >= x_net_sq + y_net_sq
-      array[y][x] = "#"
     end
-    }
-  }
+  end
+  return array
+else
+  return array
+end
 end
 
-newArray = in_circle(canvas, r, circleX, circleY)
+def square_test(array, x1, y1, x3, y3, h, w, square_details, square_drawn)
+  if w >= 10 && h >= 10 && w <= 100 && h <= 100 && x1 >= -100 && y1 >= -100 && x3 >= -100 && y3 >= -100 && x1 <= 200 && y1 <= 200 && x3 <= 200 && y3 <= 200
+    y_scanned_array = y_test(array, x1, y1, x3, y3, h, w, square_drawn)
+    x_test_array = x_test(array, x1, y1, x3, y3, h, w, square_drawn)
+    result = mid_points(x_test_array, square_details, square_drawn)
 
-in_sqr(newArray, x1, y1, x3, y3, h, w)
+    result.each do |x|
+      puts x.flatten.join
+    end
+  end
+end
+
+
+complete_square(square_details)
+array_w_cir = in_circle(canvas, r, circleX, circleY)
+square_test(array_w_cir, x1, y1, x3, y3, h, w, square_details, square_drawn)
+
+
+
